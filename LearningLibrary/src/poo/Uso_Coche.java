6bacdce8f34c89ff60e4ffcbd8cbc50f5e55c7a5
Víctor Santiago Ferrera <victor.santiago@the-cocktail.com>
2023-03-03 12:41:51 +0100

    getters y setters clase Coche()

--- LearningLibrary/src/poo/Uso_Coche.java
+++ LearningLibrary/src/poo/Uso_Coche.java
@@ -4,6 +4,6 @@ public class Uso_Coche {
     public static void main(String[] args) {
         Coche Renault = new Coche(); // INSTANCIAR UNA CLASE
 
-        System.out.println("Este coche tiene " + Renault.ruedas + " ruedas");
+        System.out.println("Este coche tiene " + Renault.getRuedas() + " ruedas");
     }
 }
