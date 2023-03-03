5f899182aa2e96b7adb8f21cd584994bb2bc0afd
Víctor Santiago Ferrera <victor.santiago@the-cocktail.com>
2023-03-03 12:31:16 +0100

    Añadida clase Coche y Uso_Coche. Next #30

---
+++ LearningLibrary/src/poo/Uso_Coche.java
@@ -0,0 +1,9 @@
+package poo;
+
+public class Uso_Coche {
+    public static void main(String[] args) {
+        Coche Renault = new Coche(); // INSTANCIAR UNA CLASE
+
+        System.out.println("Este coche tiene " + Renault.ruedas + " ruedas");
+    }
+}
