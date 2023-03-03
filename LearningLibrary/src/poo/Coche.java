28c54221d4e9584074b8dd76c90810ec3737490d
Víctor Santiago Ferrera <victor.santiago@the-cocktail.com>
2023-03-03 13:48:09 +0100

    Add getters and setters and new properties out of constructor

--- LearningLibrary/src/poo/Coche.java
+++ LearningLibrary/src/poo/Coche.java
@@ -1,48 +1,106 @@
 package poo;
 public class Coche{
-    
+    // Propiedades comunes
     private int ruedas;
     private int largo;
     private int ancho;
     private int motor;
-    private int peso;
+    private int peso_plataforma;
+    
+    // Propiedades que pueden variar
+    String color;
+    int peso_total;
+    boolean asientos_cuero, climatizador;
 
+    
+    // Constructor con los valores que nunca cambian
     public Coche(){
         ruedas = 4;
         largo = 2000;
         ancho = 300;
         motor = 1600;
-        peso = 500;
+        peso_plataforma = 500;
     }
     
+
     public int getRuedas() {
-        return ruedas;
+        return this.ruedas;
     }
-    public int getLargo() {
-        return largo;
-    }
-    public int getAncho() {
-        return ancho;
-    }
-    public int getMotor() {
-        return motor;
-    }
-    public int getPeso() {
-        return peso;
-    }
+
     public void setRuedas(int ruedas) {
         this.ruedas = ruedas;
     }
+
+    public int getLargo() {
+        return this.largo;
+    }
+
     public void setLargo(int largo) {
         this.largo = largo;
     }
+
+    public int getAncho() {
+        return this.ancho;
+    }
+
     public void setAncho(int ancho) {
         this.ancho = ancho;
     }
+
+    public int getMotor() {
+        return this.motor;
+    }
+
     public void setMotor(int motor) {
         this.motor = motor;
     }
-    public void setPeso(int peso) {
-        this.peso = peso;
+
+    public int getPeso_plataforma() {
+        return this.peso_plataforma;
     }
+
+    public void setPeso_plataforma(int peso) {
+        this.peso_plataforma = peso;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPeso_total() {
+        return this.peso_total;
+    }
+
+    public void setPeso_total(int peso_total) {
+        this.peso_total = peso_total;
+    }
+
+    public boolean isAsientos_cuero() {
+        return this.asientos_cuero;
+    }
+
+    public boolean getAsientos_cuero() {
+        return this.asientos_cuero;
+    }
+
+    public void setAsientos_cuero(boolean asientos_cuero) {
+        this.asientos_cuero = asientos_cuero;
+    }
+
+    public boolean isClimatizador() {
+        return this.climatizador;
+    }
+
+    public boolean getClimatizador() {
+        return this.climatizador;
+    }
+
+    public void setClimatizador(boolean climatizador) {
+        this.climatizador = climatizador;
+    }
+    
 }
--- LearningLibrary/src/poo/Uso_Coche.java
+++ LearningLibrary/src/poo/Uso_Coche.java
@@ -4,6 +4,9 @@ public class Uso_Coche {
     public static void main(String[] args) {
         Coche Renault = new Coche(); // INSTANCIAR UNA CLASE
 
+        Renault.setColor("Azul");
         System.out.println("Este coche tiene " + Renault.getRuedas() + " ruedas");
+        System.out.println("Este coche es de color " + Renault.getColor());
+
     }
 }
